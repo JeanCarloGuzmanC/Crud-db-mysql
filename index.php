d76b0ce2a19e09c8ed6515b6ee58f0752860708e
JeanCarloGuzmanC <111938847+JeanCarloGuzmanC@users.noreply.github.com>
2023-05-20 21:07:05 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php 
+include ('connection.php');
+
+$con = connection();
+
+$sql = "SELECT * FROM users";
+
+$query = mysqli_query($con, $sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/estilos.css">
+    <title>Register WNG</title>
+</head>
+<body>
+    <div class="users-form">
+        <form action="insert-user.php" method="POST">
+            <h1>Crea tu usuario</h1>
+            <input type="text" name="name" placeholder="Nombre">
+            <input type="text" name="lastname" placeholder="Apellido">
+            <input type="text" name="username" placeholder="Nombre de usuario">
+            <input type="text" name="password" placeholder="Contraseña">
+            <input type="text" name="email" placeholder="Email">
+
+            <input type="submit" value="Agregar Usuario">
+        </form>
+    </div>
+
+    <div class="users-table">
+        <h2>Usuario registrado</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Nombre de usuario</th>
+                    <th>Contraseña</th>
+                    <th>Email</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+
+            <?php while ($row = mysqli_fetch_array($query)): ?>
+
+                <tr>
+
+                    <th><?= $row['id'] ?></th>
+                    <th><?= $row['name'] ?></th>
+                    <th><?= $row['lastname'] ?></th>
+                    <th><?= $row['username'] ?></th>
+                    <th><?= $row['password'] ?></th>
+                    <th><?= $row['email'] ?></th>
+
+                    <th><a href="update.php?id=<?= $row['id'] ?>" class="users-table--edit">Editar</a></th>
+                    <th><a href="delete-user.php?id=<?= $row['id'] ?>" class="users-table--delete" >Eliminar</a></th>
+                </tr>
+            <?php endwhile; ?>
+            </tbody>
+        </table>
+    </div>
+
+
+</body>
+</html>
